58d426346b7fbbfb3aea53bba6fa11d8de3aeca9
Easy-Name <abilonge@yahoo.com>
2024-09-19 14:21:19 -0300

    second commit

--- Pages/Students/Index.cshtml.cs
+++ Pages/Students/Index.cshtml.cs
@@ -1,11 +1,5 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
-using Domain.Data;
 using Domain.Models;
 
 namespace Domain.Pages_Students
